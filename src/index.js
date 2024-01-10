e906fffe0d8972aa6f289eee36ef87cf5c709351
nick6850 <137728530+nick6850@users.noreply.github.com>
2024-01-10 05:17:41 +0300

    Minor import fixes in Index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const db = require("./config/dbConfig");
 const userRouter = require("./routes/userRoutes");
+const profileRouter = require("./routes/profileRoutes");
 const path = require("path");
+const authMiddleware = require("./middlewares/authMiddleware");
+const profileController = require("./controllers/profileController");
 
 // Express app
 const app = express();
@@ -15,11 +18,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-// Serve static HTML file
+// Serve static HTML file for testing (you can also use Postman or Insomnia)
 app.use("/", express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use("/user", userRouter);
+app.use("/profile", profileRouter);
+app.use("/profiles", authMiddleware, profileController.getAllProfiles);
 
 // Synchronize Database
 db.sync({ force: false })
